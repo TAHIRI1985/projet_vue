a5a17fd85f22d51edef6aa5827197a8ee2259a0c
TAHIRI1985 <113614023+TAHIRI1985@users.noreply.github.com>
2022-11-13 01:27:58 +0100

    install environement

--- bijouxsiam/src/main.js
+++ bijouxsiam/src/main.js
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import * as bootstrap from "bootstrap";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(router).use(bootstrap).mount("#app");
