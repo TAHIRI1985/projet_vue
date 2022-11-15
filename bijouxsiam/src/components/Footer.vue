225dec3b3298c716cb9ad6b395ae3d3bbdb9e948
TAHIRI1985 <113614023+TAHIRI1985@users.noreply.github.com>
2022-11-15 22:24:10 +0100

    ...

---
+++ bijouxsiam/src/components/Footer.vue
@@ -0,0 +1,124 @@
+<template>
+    <footer>
+<section class="newsletter">
+        <div class="container">
+        <div class="row">
+        <div class="col-sm-12">
+          <div class="content">
+            <h2>Inscription à la newsletter</h2>
+            <p>Promotions, nouveaux produits et soldes. Directement dans votre boîte de réception.</p>
+          <div class="input-group">
+                 <input type="email" class="form-control" placeholder="Adresse E-mail">
+                 <span class="input-group-btn">
+                 <button class="btn" type="submit"> <b> Je m'abonne</b> </button>
+                 </span>
+                  </div>
+          </div>
+        </div>
+        </div>
+        </div>
+        </section>
+      <div class="container-fluid">
+       <div class=" condition container">
+        <div class=" foot row align-self-center  ">
+        
+       <div id="recherche" class="col-xs-12 col-ms-12 col-md-5 col-lg-5"> <a href="#">Recherche</a></div><br>
+       <div class="col-xs-12 col-ms-12 col-md-2 col-lg-2"><a href="#">Cantactez-nous</a></div><br>
+       <div id="conditions"  class="col-xs-12 col-ms-12 col-md-5 col-lg-5"> <a href="#">Conditions Générales</a></div><br>
+       <div class="col-12"><br> </div>
+         <div class="col-xs-12 col-ms-12 col-md-4 col-lg-4">  
+            <span class="sociale ">
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+             </span>
+         </div>    
+            
+
+             <div class="col-xs-12 col-ms-12 col-md-4 col-lg-4">
+             <span class="copie">
+             <i class="fa-regular fa-copyright"></i> 2022,<a href="#"> Bijoux Siam</a>
+             </span>
+            </div><br>
+
+             <div class="col-xs-12 col-ms-12 col-md-4 col-lg-4">
+             <span class="bank">
+                <img src="css/images/moyen-paiement-prochasse-paypal_2-removebg-preview.png" alt="carte "  width="200" height="50">
+            </span>
+            </div><br>
+
+
+          </div>
+      </div>
+      </div>
+    </footer>
+</template>
+<script>
+
+
+
+
+
+
+</script>
+
+<style>
+footer {
+  background-color:#fff;
+  color: gray;
+  height: auto;
+  margin: 0px 20px 20px 20px;
+  padding: 50px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 40px grey;
+}
+.condition a {
+  color: gray;
+  text-decoration: none;
+}
+.condition {
+  height: 200px;
+}
+.foot {
+  padding-top: 50px;
+  padding-bottom: 50px;
+}
+.newsletter {
+  padding: 50px 0;
+}
+
+.newsletter .content {
+  max-width: 550px;
+  margin: 0 auto;
+  text-align: center;
+  position: relative;
+  z-index: 2;
+}
+.newsletter .content h2 {
+  color: gray;
+  margin-bottom: 20px;
+}
+.newsletter .content .form-control {
+  height: 50px;
+  font-size: 12px;
+
+  background-color: rgba(128, 128, 128, 0.548);
+  border-radius: 5px;
+}
+.newsletter .content.form-control:focus {
+  box-shadow: none;
+  border: 2px solid gray;
+}
+.newsletter .content .btn {
+  min-height: 50px;
+  border-radius: 5px;
+  background: #000;
+  color: #fff;
+  font-weight: 600;
+}
+
+
+
+
+
+</style>
---
+++ bijouxsiam/src/components/Header.vue
@@ -0,0 +1,256 @@
+<template>
+ <header>
+      <!-- place navbar here -->
+      <nav class="navbar ">
+        <div class=" topnavbar container-fluid">
+          <button
+            class="navbar-toggler bmenu"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="offcanvas offcanvas-start divmenu"
+            tabindex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="offcanvas-body ">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#"
+                    ><b>Acceuil</b></a
+                  >
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"><b>Cartes Cadeaux</b></a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <b>Bijoux</b>
+                  </a>
+                  <ul class="dropdown-menu bBijoux">
+                    <li><a class="dropdown-item" href="#">Collier</a></li>
+                    <li>
+                      <a class="dropdown-item" href="#">Bague</a>
+                    </li>
+
+                    <li>
+                      <a class="dropdown-item" href="#">B.d'orielle</a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">Bracelet</a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">Montre</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <br />
+              <div class="conn">
+                <ul>
+                  <li>
+                    <a class="connecte" href="#">Se connecte</a>
+                  </li>
+                  <li>
+                    <a class="connecte" href="#">Créer un compte</a>
+                  </li>
+                  <li>
+                    <a class="connecte" href="#">Contacter-nous</a>
+                  </li>
+                  <li>
+                    <a class="connecte" href="#">Condition Générales</a>
+                  </li>
+                </ul>
+              </div>
+              <br />
+              <br />
+
+              <footera class="sc">
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+              <!-- </footera>
+              <form class="d-flex mt-3" role="search">
+                <input
+                  class="form-control me-2"
+                  type="search"S
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button class="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form> -->
+            </div>
+          </div>
+           <img src="css/images/logo2.png" alt="logo" width="250" height="200" /> 
+          <div  >
+          <span>
+            <a class="bsearch" href="#"
+              ><i class="fa-solid fa-magnifying-glass"></i
+            > </a>
+          </span>
+           <span>
+            <a class="bpanier" href="#"
+              ><i class="fa-solid fa-bag-shopping"></i
+            >   <span id="notification">{{ajout}}</span> 
+              
+             <span id="prixtotal">{{ totalPrix.toFixed(2) }} €</span> 
+              
+              </a>
+          </span>
+        </div>
+        </div>
+        <div class="container  justify-content-center" class="home" > <p class="item"> Home</p>  <p class="item"> Bijoux </p>   <p class="item"> Cartes Cadeaux </p>  </div>
+         
+        <div class="carosel container-fluid ">
+          <div class="divcarosel  container-fluid ">
+            <div
+              id="carouselExampleDark"
+              class="carousel carousel-dark slide "
+              data-bs-ride="carousel"
+              
+            >
+              <div class="carousel-indicators">
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleDark"
+                  data-bs-slide-to="0"
+                  class="active"
+                  aria-current="true"
+                  aria-label="Slide 1"
+                ></button>
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleDark"
+                  data-bs-slide-to="1"
+                  aria-label="Slide 2"
+                ></button>
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleDark"
+                  data-bs-slide-to="2"
+                  aria-label="Slide 3"
+                ></button>
+              </div>
+              <div class=" divcarousel carousel-inner">
+                <div class="carousel-item active" data-bs-interval="10000">
+                  <img
+                    src="css/images/2+.png"
+                    class="d-block w-100 imgcarosel"
+                    alt="colier"
+                    width=""
+                    height="500"
+                  />
+                  <div class="carousel-caption d-none d-md-block    ">
+                    
+                       <h5> <a href="#"><b>Collier</b></a> </h5>
+                      
+                  </div>
+                </div>
+                 
+                <div class="carousel-item" data-bs-interval="2000">
+                  
+                  <img
+                    src="css/images/6.jpg"
+                    class="d-block w-100 imgcarosel"
+                    alt="b.orielle"
+                    width=""
+                    height="500"
+                  />
+                  <div class="carousel-caption d-none d-md-block">
+                  
+                    <h5> <a href="#"><b>B.d'orielle</b></a> </h5>
+                    
+                  </div>
+                </div>
+                <div class="carousel-item ">
+                  <img
+                    src="css/images/7.jpg"
+                    class="d-block w-100 imgcarosel"
+                    alt="bague"
+                    width=""
+                    height="500"
+                  />
+                  <div class="carousel-caption d-none d-md-block  ">
+                    <h5 > <a href="#"><B>Bague</B></a> </h5>
+                    
+                  </div>
+                </div>
+              </div>
+              <button
+                class="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleDark"
+                data-bs-slide="prev"
+              >
+                <span
+                  class="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button
+                class="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleDark"
+                data-bs-slide="next"
+              >
+                <span
+                  class="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        
+         
+        </div>
+        </div>
+      </nav>
+    </header>
+
+</template>
+
+
+<script>
+
+
+
+
+
+
+</script>
+
+
+
+
+<style>
+
+
+
+
+
+</style>
